bc5bd167dcc781cc265c3618c585f37f0af5d114
macroapp <pietro.poloni18@gmail.com>
2025-09-23 23:23:37 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import ATPCalendar from './_components/ATPCalendar'
+import AtpCalendar from '@/components/AtpCalendar';
 
 export default function Home() {
   return (
     <main>
       <h1>Fanta Tennis</h1>
       <p>Welcome! Use Sign Up / Sign In to get started.</p>
-      <ATPCalendar />
+      <AtpCalendar />
     </main>
   );
 }
